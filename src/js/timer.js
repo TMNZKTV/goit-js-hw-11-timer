d53a7a9dfab65f505fc2e0f5064aebad8ebb64bc
TUMEN ZORIKTUEV <69411964+TMNZKTV@users.noreply.github.com>
2021-02-06 19:59:15 +0800

    changes according to mmentor's comments

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,79 +1,92 @@
+// Создаем разметку. Создаем массив строк для дальнейшей работы.
+const timers = ['Days', 'Hours', 'Minutes', 'Seconds'];
+
+// Функция для создания разметки
+const createItem = timer => {
+  const itemRef = document.createElement('div');
+  itemRef.classList.add('field');
+  itemRef.insertAdjacentHTML('afterbegin', `<span class="value" data-value="${timer.toLowerCase()}"></span>`);
+  itemRef.insertAdjacentHTML('beforeend', `<span class="label">${timer}</span>`);
+  return itemRef;
+};
+
+// Создаем перменную-массив, которая содержит в себе результат функции createItem
+const mappedTimers = timers.map(timer => createItem(timer));
+
+// Создаем начальный div и помещаем переменную-массив
+const divRef = document.createElement('div');
+divRef.classList.add('timer');
+divRef.id = '#timer-1';
+divRef.append(...mappedTimers);
+
+// Собранную структуру за один раз добавляем в ДОМ
+const body = document.querySelector('body');
+body.insertAdjacentElement('afterbegin', divRef);
+
+// Выбираем поля для дальнейшего заполнения
+const refs = {
+  daysTimer: document.querySelector('[data-value="days"]'),
+  hoursTimer: document.querySelector('[data-value="hours"]'),
+  minutesTimer: document.querySelector('[data-value="minutes"]'),
+  secondsTimer: document.querySelector('[data-value="seconds"]'),
+};
+
+// Создаем класс
 class CountdownTimer {
-    constructor({ selector, targetDate }) {
-      this.selector = selector;
-      this.targetDate = targetDate;
-    }
-  
-    createMarkup() {
-      const timers = ["Days", "Hours", "Minutes", "Seconds"];
-  
-      const createItem = (timer) => {
-        const itemRef = document.createElement("div");
-        itemRef.classList.add("field");
-        itemRef.insertAdjacentHTML(
-          "afterbegin",
-          `<span class="value" data-value="${timer.toLowerCase()}"></span>`
-        );
-        itemRef.insertAdjacentHTML(
-          "beforeend",
-          `<span class="label">${timer}</span>`
-        );
-        return itemRef;
-      };
-      const mappedTimers = timers.map((timer) => createItem(timer));
-  
-      const divRef = document.createElement("div");
-      divRef.classList.add("timer");
-      divRef.id = "#timer-1";
-      divRef.append(...mappedTimers);
-  
-      const body = document.querySelector("body");
-      body.insertAdjacentElement("afterbegin", divRef);
-    }
-  
-    start() {
-      const eventTime = this.targetDate.getTime();
-  
-      setInterval(() => {
-        const currentTime = Date.now();
-  
-        const deltaTime = eventTime - currentTime;
-  
-        this.updateClockFace(deltaTime);
-      }, 1000);
-    }
-  
-    updateClockFace(time) {
-      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-      const hours = this.pad(
-        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      );
-      const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-  
-      const refs = {
-        daysTimer: document.querySelector('[data-value="days"]'),
-        hoursTimer: document.querySelector('[data-value="hours"]'),
-        minutesTimer: document.querySelector('[data-value="minutes"]'),
-        secondsTimer: document.querySelector('[data-value="seconds"]'),
-      };
-  
-      refs.daysTimer.textContent = `${days}`;
-      refs.hoursTimer.textContent = `${hours}`;
-      refs.minutesTimer.textContent = `${mins}`;
-      refs.secondsTimer.textContent = `${secs}`;
-    }
-  
-    pad(value) {
-      return String(value).padStart(2, "0");
-    }
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
   }
-  
-  const timer = new CountdownTimer({
-    selector: "#timer-1",
-    targetDate: new Date("Apr 5, 2021"),
-  });
-  
-  timer.createMarkup();
-  timer.start();
-  
+
+  start() {
+    const eventTime = this.targetDate.getTime();
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = eventTime - currentTime;
+      calculateTime(deltaTime);
+    }, 1000);
+  }
+}
+
+// Создаем таймер с нужной датой
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Mar 8, 2021'),
+});
+
+// Запускаем таймер
+timer.start();
+
+// Объявляем переменные для дальнейшей работы с ними
+let days;
+let hours;
+let mins;
+let secs;
+
+// Из deltaTime высчитаем время для отображения
+function calculateTime(time) {
+  days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  updateClockFace(days);
+
+  hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  updateClockFace(hours);
+
+  mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  updateClockFace(mins);
+
+  secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  updateClockFace(secs);
+}
+
+// Функция для отображения чисел в виде "00"
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+// Функция отображения чисел в DOM
+function updateClockFace(value) {
+  refs.daysTimer.textContent = `${days}`;
+  refs.hoursTimer.textContent = `${hours}`;
+  refs.minutesTimer.textContent = `${mins}`;
+  refs.secondsTimer.textContent = `${secs}`;
+}
